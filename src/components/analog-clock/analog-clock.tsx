48801b1e6df5c269d5ce754c29d5d12bc4a70d14
LuKaas <lukaas.blu@gmail.com>
2019-06-04 20:47:28 +0000

    Make the clock tick

--- src/components/analog-clock/analog-clock.tsx
+++ src/components/analog-clock/analog-clock.tsx
@@ -1,13 +1,39 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, State } from '@stencil/core';
 
 @Component({
   tag: 'analog-clock'
 })
 export class AnalogClock {
+  timer: number;
+
+  @State() time: number = Date.now();
+
+  componentDidLoad() {
+    this.timer = window.setInterval(() => {
+      this.time = Date.now();
+    }, 250);
+  }
+
+  componentDidUnload() {
+    clearInterval(this.timer);
+  }
+
+  get hours(): number {
+    return new Date().getHours();
+  }
+
+  get minutes(): number {
+    return new Date().getMinutes();
+  }
+
+  get seconds(): number {
+    return new Date().getSeconds();
+  }
+
   render() {
     return (
       <div>
-        <clock-display hours={8} minutes={21} seconds={29}/>
+        <clock-display hours={this.hours} minutes={this.minutes} seconds={this.seconds}/>
       </div>
     );
   }
